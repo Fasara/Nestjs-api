f67fb2b33be89e67e6cd1cddbed8d78a34ac8db8
Fabiana <fabiana.asara@gmail.com>
2021-12-10 16:51:12 +0000

    testing endpoints

--- src/course/controller/course.controller.ts
+++ src/course/controller/course.controller.ts
@@ -1,7 +1,33 @@
+import { Controller, Get, Post, Put, Param, Body } from '@nestjs/common';
 
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('course')
+@Controller('courses')
 export class CourseController {
+    @Get()
+    getCourses(){
+        return "All courses"
+        //extract the actual courses
+    }
 
+    @Get('/:courseId')
+    getCourseById( @Param('courseId') courseId : number
+    ) {
+        console.log(courseId);
+        return `Get course with Id of ${courseId}`
+    }
+
+    @Post()
+    addCourse(
+        @Body() body
+    ) {
+        console.log(body)
+        return "Add new course"
+    }
+
+    @Put('/:courseId')
+    updateCourses(){
+        return "Update courses by id"
+        //add body
+    }
 }
+
+// TO-DO: get courses funciton => retrieve data from the database

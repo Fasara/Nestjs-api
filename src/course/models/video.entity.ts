156bb166878c564184e8e2291705d4ce15514678
Fabiana <fabiana.asara@gmail.com>
2022-03-04 15:23:31 +0000

    Change of plan with db design

---
+++ src/course/models/video.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Course } from "./course.entity";
+
+
+@Entity('video')
+export class VideoEntity {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    url: 'text';
+}

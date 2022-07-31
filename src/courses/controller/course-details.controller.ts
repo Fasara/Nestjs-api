7852d473cb10439d48db019dea432134703aafcd
Fabiana <fabiana.asara@gmail.com>
2022-07-31 19:39:57 +0100

    Installs jest

---
+++ src/courses/controller/course-details.controller.ts
@@ -0,0 +1,8 @@
+// import { Controller, Get, Post, Patch, Param, Body, Delete } from '@nestjs/common';
+// import { VideoEntity } from '../videos/models/video.entity';
+
+
+// @Controller('course-details'){
+
+
+// }
